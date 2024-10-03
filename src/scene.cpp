4a2d6e77e0de68d07a2bbee8880ba8e87dd17579
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2024-10-04 01:56:41 +0300

    first triangle

--- src/scene.cpp
+++ src/scene.cpp
@@ -8,79 +8,63 @@
 #include <SFML/OpenGL.hpp>
 
 GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path){
-
-    // Создаем шейдеры
     GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
     GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
 
-    // Загружаем код Вершинного Шейдера из файла
     std::string VertexShaderCode;
     std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
-    if(VertexShaderStream.is_open())
-    {
+    if (VertexShaderStream.is_open()) {
         std::stringstream sstr;
         sstr << VertexShaderStream.rdbuf();
         VertexShaderCode = sstr.str();
-        std::cerr << VertexShaderCode << "\n";
         VertexShaderStream.close();
     }
 
-    // Загружаем код Фрагментного шейдера из файла
     std::string FragmentShaderCode;
     std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
-    if(FragmentShaderStream.is_open()){
+    if (FragmentShaderStream.is_open()) {
         std::stringstream sstr;
         sstr << FragmentShaderStream.rdbuf();
         FragmentShaderCode = sstr.str();
-        std::cerr << FragmentShaderCode << "\n";
         FragmentShaderStream.close();
     }
 
     GLint Result = GL_FALSE;
     int InfoLogLength;
 
-    // Компилируем Вершинный шейдер
-    printf("Компиляция шейдера: %s\n", vertex_file_path);
     char const * VertexSourcePointer = VertexShaderCode.c_str();
     glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
     glCompileShader(VertexShaderID);
 
-    // Выполняем проверку Вершинного шейдера
     glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
     glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    if ( InfoLogLength > 0 ){
+    if (InfoLogLength > 0) {
       std::vector<char> VertexShaderErrorMessage(InfoLogLength+1);
       glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
     }
 
-    // Компилируем Фрагментный шейдер
-    printf("Компиляция шейдера: %s\n", fragment_file_path);
     char const * FragmentSourcePointer = FragmentShaderCode.c_str();
     glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
     glCompileShader(FragmentShaderID);
 
-    // Проверяем Фрагментный шейдер
     glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
     glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    if ( InfoLogLength > 0 ){
+    if (InfoLogLength > 0) {
       std::vector<char> FragmentShaderErrorMessage(InfoLogLength+1);
       glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
     }
 
-    // Создаем шейдерную программу и привязываем шейдеры к ней
-    fprintf(stdout, "Создаем шейдерную программу и привязываем шейдеры к ней\n");
     GLuint ProgramID = glCreateProgram();
     glAttachShader(ProgramID, VertexShaderID);
     glAttachShader(ProgramID, FragmentShaderID);
     glLinkProgram(ProgramID);
 
-    // Проверяем шейдерную программу
     glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
     glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    if ( InfoLogLength > 0 ){
+    if (InfoLogLength > 0) {
       std::vector<char> ProgramErrorMessage(InfoLogLength+1);
       glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
-      fprintf(stdout, "%s\n", &ProgramErrorMessage[0]);
+      std::cout << &ProgramErrorMessage[0] << "\n";
     }
 
     glDeleteShader(VertexShaderID);
@@ -114,17 +98,13 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(posAttrID, 2, GL_FLOAT, GL_FALSE, 0, vertices); // 0);
-    glEnableVertexAttribArray(posAttrID);
-
     GLuint programID = LoadShaders("src/vertexshader", "src/fragmentshader");
-    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
     glUseProgram(programID);
-    GLint posAttrID = glGetAttribLocation(programID, "position");
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
-    glVertexAttribPointer(posAttrID, 2, GL_FLOAT, GL_FALSE, 0, vertices); // 0);
-    glEnableVertexAttribArray(posAttrID);
-
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
