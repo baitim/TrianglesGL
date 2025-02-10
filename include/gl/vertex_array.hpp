27f94bfbc3c08c489572da910073db519ce78239
Timur Baidusenov <baidiusenov.tb@phystech.edu>
2025-02-10 03:33:28 +0300

    refactor copy ctor

--- include/gl/vertex_array.hpp
+++ include/gl/vertex_array.hpp
@@ -14,14 +14,45 @@ namespace vertex_array {
             glDeleteBuffers(1, &VBO_);
         }
 
+        void bind_VAO_VBO() {
+            glGenVertexArrays(1, &VAO_);
+            glGenBuffers(1, &VBO_);
+        
+            glBindVertexArray(VAO_);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO_);
+        }
+
+        void set_VAO() {
+            glEnableVertexAttribArray(0);
+            glEnableVertexAttribArray(1);
+            glEnableVertexAttribArray(2);
+            
+            using data = scene::vertex2render_t;
+            size_t vertex_size = sizeof(data);
+            void* coord_offset   = std::bit_cast<void*>(offsetof(data, coord));
+            void* normal_offset  = std::bit_cast<void*>(offsetof(data, normal));
+            void* color_offset   = std::bit_cast<void*>(offsetof(data, color));
+            glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, vertex_size, coord_offset);
+            glVertexAttribPointer (1, 3, GL_FLOAT, GL_FALSE, vertex_size, normal_offset);
+            glVertexAttribIPointer(2, 1, GL_BYTE,            vertex_size, color_offset);
+        }
+
     public:
         vertex_array_t() {}
 
         vertex_array_t(const vertex_array_t& rhs) {
-            glGenVertexArrays(1, &VAO_);
-            glGenBuffers(1, &VBO_);
-            glBindVertexArray(VAO_);
-            glBindBuffer(GL_ARRAY_BUFFER, VBO_);
+            bind_VAO_VBO();
+
+            glBindVertexArray(rhs.VAO_);
+            glBindBuffer(GL_ARRAY_BUFFER, rhs.VBO_);
+        
+            GLint buffer_size = 0;
+            glGetBufferParameteriv(GL_ARRAY_BUFFER, GL_BUFFER_SIZE, &buffer_size);
+        
+            glBufferData(GL_ARRAY_BUFFER, buffer_size, nullptr, GL_STATIC_DRAW);
+            glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_ARRAY_BUFFER, 0, 0, buffer_size);
+        
+            set_VAO();
         }
 
         vertex_array_t& operator=(const vertex_array_t& rhs) {
@@ -47,24 +78,10 @@ namespace vertex_array {
 
             size_t vertex_size = sizeof(scene::vertex2render_t);
 
-            glGenVertexArrays(1, &VAO_);
-            glGenBuffers(1, &VBO_);
-            glBindVertexArray(VAO_);
-            glBindBuffer(GL_ARRAY_BUFFER, VBO_);
+            bind_VAO_VBO();
             glBufferData(GL_ARRAY_BUFFER, vertices.size() * vertex_size,
                          vertices.data(), GL_STATIC_DRAW);
-
-            glEnableVertexAttribArray(0);
-            glEnableVertexAttribArray(1);
-            glEnableVertexAttribArray(2);
-
-            using data = scene::vertex2render_t;
-            void* coord_offset   = std::bit_cast<void*>(offsetof(data, coord));
-            void* normal_offset  = std::bit_cast<void*>(offsetof(data, normal));
-            void* color_offset   = std::bit_cast<void*>(offsetof(data, color));
-            glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, vertex_size, coord_offset);
-            glVertexAttribPointer (1, 3, GL_FLOAT, GL_FALSE, vertex_size, normal_offset);
-            glVertexAttribIPointer(2, 1, GL_BYTE,            vertex_size, color_offset);
+            set_VAO();
         }
 
         ~vertex_array_t() {
